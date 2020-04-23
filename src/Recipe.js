0608014b8802d4a747865f8e93c7a62a5ef94b3b
Jay <jordanchu95@gmail.com>
2020-04-23 17:47:56 -0400

    uh

--- src/Recipe.js
+++ src/Recipe.js
@@ -3,10 +3,10 @@ import style from './recipe.module.css';
 
 const Recipe = ({title, calories, image, ingredients}) => {
     return(
-        <div>
+        <div className={style.recipe}>
             <h1>{title}</h1>
             <p>{calories}</p>
-            <img src={image} alt="" />
+            <img className={style.image} src={image} alt="" />
             <ol>
                 {ingredients.map(ingredient => (
                     <li>{ingredient.text}</li>
