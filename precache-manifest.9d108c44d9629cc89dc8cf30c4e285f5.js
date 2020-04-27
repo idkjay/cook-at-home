2316780c27fe1c17f81a6e508b1e2c895304cde9
Jay <jordanchu95@gmail.com>
2020-04-26 23:03:57 -0400

    Updates

---
+++ precache-manifest.9d108c44d9629cc89dc8cf30c4e285f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca08d0492ad7707b748a2b3e20444aab",
+    "url": "/cook-at-home/index.html"
+  },
+  {
+    "revision": "0f26434b120761ceb76b",
+    "url": "/cook-at-home/static/css/main.2aed83b8.chunk.css"
+  },
+  {
+    "revision": "acac99d170f60351ec04",
+    "url": "/cook-at-home/static/js/2.2a833dc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cook-at-home/static/js/2.2a833dc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f26434b120761ceb76b",
+    "url": "/cook-at-home/static/js/main.b31bbf09.chunk.js"
+  },
+  {
+    "revision": "b423fc4da9b2299a2d61",
+    "url": "/cook-at-home/static/js/runtime-main.ea439f22.js"
+  }
+]);
