5d108ba27a1c0c23787218f04000a706f81fc091
Jay <jordanchu95@gmail.com>
2020-06-11 15:38:49 -0400

    join function on array

--- src/RecipeInfo.js
+++ src/RecipeInfo.js
@@ -3,9 +3,9 @@ import React from 'react';
 const RecipeInfo =({ dietLabels, healthLabels, calories }) => {
     return (
         <ul className="ingredient-list">
-            <li>Health Labels: {healthLabels}</li>
-            <li>Diet Labels: {dietLabels}</li>
-            <li>Total Calories: {calories}</li>
+            <li className="ingredient-text">Health Labels: {healthLabels.join(', ')}</li>
+            <li className="ingredient-text">Diet Labels: {dietLabels.join(', ')}</li>
+            <li className="ingredient-text">Total Calories: {calories.toFixed(2)}</li>
         </ul>
     )
 
