5817941eb7e5005c147e92803fc0b4b72db8f2d5
Jay <jordanchu95@gmail.com>
2020-06-12 11:32:04 -0400

    strong tags

--- src/RecipeInfo.js
+++ src/RecipeInfo.js
@@ -3,9 +3,9 @@ import React from 'react';
 const RecipeInfo =({ dietLabels, healthLabels, calories }) => {
     return (
         <ul className="ingredient-list">
-            <li className="ingredient-text">Health Labels: {healthLabels.join(', ')}</li>
-            <li className="ingredient-text">Diet Labels: {dietLabels.join(', ')}</li>
-            <li className="ingredient-text">Total Calories: {calories.toFixed(2)}</li>
+            <li className="ingredient-text"><strong>Health Labels:</strong> {healthLabels.join(', ')}</li>
+            <li className="ingredient-text"><strong>Diet Labels:</strong> {dietLabels.join(', ')}</li>
+            <li className="ingredient-text"><strong>Total Calories:</strong> {calories.toFixed(2)}</li>
         </ul>
     )
 
