933f827f7e5ebb98bc247999b8a4cea5eaf6bff3
Jay <jordanchu95@gmail.com>
2020-06-12 12:49:22 -0400

    packages

--- src/Footer.js
+++ src/Footer.js
@@ -1,48 +1,47 @@
 import React from 'react';
-import "./Footer.css"
+import "./Footer.css";
 
 const Footer = () => {
-    return(
-        <div className="main-footer">
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <h4>pls work</h4>
-                        <ul className="list-unstyled">
-                            <li>dsadsad</li>
-                            <li>dsadsad</li>
-                            <li>dsadsad</li>
-                        </ul>
-                    </div>
 
-                    <div className="col">
-                        <h4>pls work 2</h4>
-                        <ul className="list-unstyled">
-                            <li>dsadsad</li>
-                            <li>dsadsad</li>
-                            <li>dsadsad</li>
-                        </ul>
-                    </div>
+    // const github = <FontAwsomeIcon github={fa-github-square} />
+    // const linkedin = <FontAwsomeIcon linkedin={fa-linkedin-square} />
+    // const code = <FontAwsomeIcon code={fa-code} />
 
-                    <div className="col">
-                        <h4>pls work 3</h4>
-                        <ul className="list-unstyled">
-                            <li>dsadsad</li>
-                            <li>dsadsad</li>
-                            <li>dsadsad</li>
-                        </ul>
-                    </div>
-                </div>
-                <hr />
-                <div className="row">
-                    <p className="col-sm">
-                        &copy;{new Date().getFullYear()} Jordan Chu
-                    </p>
-
-                </div>
+    return (
+      <div className="main-footer">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h4>
+                Practice social distancing during the pandemic. Cook at home
+                while you stay at home! This app will help you find recipes
+                based off food and ingredients you already have in your pantry.
+              </h4>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="https://www.linkedin.com/in/jordanchu1995/">
+                    Linkedin
+                  </a>
+                </li>
+                <li>
+                  <a href="https://github.com/idkjay">GitHub</a>
+                </li>
+                <li>
+                  <a href="http://jordanchu.xyz/">Portfolio</a>
+                </li>
+              </ul>
             </div>
+          </div>
+          <hr />
+          <div className="row">
+            <p className="col-sm">
+              &copy;{new Date().getFullYear()}{" "}
+              <a href="http://jordanchu.xyz/">Jordan Chu</a>
+            </p>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Footer;
