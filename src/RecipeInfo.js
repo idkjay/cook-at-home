40566bc67af09c18a860e98b9374602458a179a2
Jay <jordanchu95@gmail.com>
2020-06-13 14:58:49 -0400

    more media query

--- src/RecipeInfo.js
+++ src/RecipeInfo.js
@@ -3,9 +3,9 @@ import React from 'react';
 const RecipeInfo =({ dietLabels, healthLabels, calories }) => {
     return (
         <ul className="info-list">
-            <li className="ingredient-text"><strong>Health Labels:</strong> {healthLabels.join(', ')}</li>
-            <li className="ingredient-text"><strong>Diet Labels:</strong> {dietLabels.join(', ')}</li>
-            <li className="ingredient-text"><strong>Total Calories:</strong> {calories.toFixed(2)}</li>
+            <li className="info-text"><strong>Health Labels:</strong> {healthLabels.join(', ')}</li>
+            <li className="info-text"><strong>Diet Labels:</strong> {dietLabels.join(', ')}</li>
+            <li className="info-text"><strong>Total Calories:</strong> {calories.toFixed(2)}</li>
         </ul>
     )
 
